7da296ebfacb5639c1b74d07fa6c9b2b0e6d083d
dima1997 <franki.dimaria@gmail.com>
2025-04-15 04:12:38 -0300

    refactor: create MercadoPagoNotificationsService

--- app/api/routes/notifications.py
+++ app/api/routes/notifications.py
@@ -1,13 +1,11 @@
-import logging
 from typing import Any
 
 from fastapi import APIRouter, Request, Response, status
 
 from app.core.config import settings
-from app.services.mercado_pago_service import MercadoPagoNotificationService
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from app.services.mercado_pago_notifications_service import (
+    MercadoPagoNotificationsService,
+)
 
 mp_sdk = settings.MERCADO_PAGO_SDK
 
@@ -21,43 +19,5 @@ router = APIRouter()
 async def mercadopago_notify(
     request: Request,
 ) -> Any:
-    default_response = Response(status_code=status.HTTP_200_OK)
-
-    MercadoPagoNotificationService().verify_request(request)
-
-    body = await request.json()
-
-    ressource_id = body["data"]["id"]
-    if ressource_id == settings.MERCADO_PAGO_NOTIFICATION_TEST_ID:
-        return default_response
-
-    notification_type = body["type"]
-
-    merchant_order_id = None
-    if notification_type == "topic_merchant_order_wh":
-        merchant_order_id = ressource_id
-    elif notification_type == "payment":
-        payment_response = mp_sdk.payment().get(ressource_id)
-        payment = payment_response["response"]
-        merchant_order_id = payment["order"]["id"]
-    else:
-        logger.info(f"{request.query_params = }")
-        logger.info(f"{request.headers = }")
-        logger.info(f"{await request.json() = }")
-
-    if merchant_order_id:
-        merchant_order_response = mp_sdk.merchant_order().get(merchant_order_id)
-        merchant_order = merchant_order_response["response"]
-
-        total_paid = 0
-        for payment in merchant_order["payments"]:
-            if payment["status"] == "approved":
-                total_paid += payment["transaction_amount"]
-
-        item_title = merchant_order["items"][0]["title"]
-        if total_paid >= merchant_order["total_amount"]:
-            logger.info(f"[Merchant Order] '{item_title}': DONE")
-        else:
-            logger.info(f"[Merchant Order] '{item_title}': WIP")
-
-    return default_response
+    await MercadoPagoNotificationsService().process_request(request)
+    return Response(status_code=status.HTTP_200_OK)
--- app/services/mercado_pago_notifications_service.py
+++ app/services/mercado_pago_notifications_service.py
@@ -1,5 +1,6 @@
 import hashlib
 import hmac
+import logging
 import urllib.parse
 
 from fastapi import Request
@@ -7,8 +8,13 @@ from fastapi import Request
 from app.core.config import settings
 from app.utilities.exceptions import NotAuthorizedException
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-class MercadoPagoNotificationService:
+mp_sdk = settings.MERCADO_PAGO_SDK
+
+
+class MercadoPagoNotificationsService:
     def verify_request(self, request: Request) -> None:
         # Obtain the x-signature value from the header
         xSignature = request.headers.get("x-signature")
@@ -55,3 +61,41 @@ class MercadoPagoNotificationService:
         if sha != hash:
             # HMAC verification failed
             raise NotAuthorizedException()
+
+    async def process_request(self, request: Request) -> None:
+        self.verify_request(request)
+
+        body = await request.json()
+
+        ressource_id = body["data"]["id"]
+        if ressource_id == settings.MERCADO_PAGO_NOTIFICATION_TEST_ID:
+            return
+
+        notification_type = body["type"]
+
+        merchant_order_id = None
+        if notification_type == "topic_merchant_order_wh":
+            merchant_order_id = ressource_id
+        elif notification_type == "payment":
+            payment_response = mp_sdk.payment().get(ressource_id)
+            payment = payment_response["response"]
+            merchant_order_id = payment["order"]["id"]
+        else:
+            logger.info(f"{request.query_params = }")
+            logger.info(f"{request.headers = }")
+            logger.info(f"{await request.json() = }")
+
+        if merchant_order_id:
+            merchant_order_response = mp_sdk.merchant_order().get(merchant_order_id)
+            merchant_order = merchant_order_response["response"]
+
+            total_paid = 0
+            for payment in merchant_order["payments"]:
+                if payment["status"] == "approved":
+                    total_paid += payment["transaction_amount"]
+
+            item_title = merchant_order["items"][0]["title"]
+            if total_paid >= merchant_order["total_amount"]:
+                logger.info(f"[Merchant Order] '{item_title}': DONE")
+            else:
+                logger.info(f"[Merchant Order] '{item_title}': WIP")
